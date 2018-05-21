a6914c74e1c2a981d159c315997321d8516a5094
Evgueni Souleimanov <esoule@100500.ca>
2018-05-20 21:53:23 -0400

    Create users' home directories with 0750 permission by default

---
+++ scripts/parts-post-install-ubuntu.d/110-adduser.conf.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+PROJECT_HOME="$( cd "$( dirname "$0" )" && cd ../.. && pwd )"
+cd "${PROJECT_HOME}"
+source "./scripts/inc-sh/common-pre.inc.sh"
+require_root_or_exit
+
+main_func()
+{
+	local orig_cfg=/etc/adduser.conf
+	local tmp_cfg=/etc/adduser.conf.001.tmp
+
+	cp ${orig_cfg} ${tmp_cfg}
+
+	sed -i -e 's/^\s*DIR_MODE=0[0-7][0-7][0-7]\s*$/DIR_MODE=0750/g' ${tmp_cfg}
+
+	chmod 0644 ${tmp_cfg}
+	chown root:root ${tmp_cfg}
+
+	diff -q ${orig_cfg} ${tmp_cfg} >/dev/null
+	local rv=$?
+
+	if [ "${rv}" = 1 ] ; then
+		mv ${tmp_cfg} ${orig_cfg}
+	fi
+
+	rm -f ${tmp_cfg}
+
+	true
+}
+
+source "./scripts/inc-sh/common-run-main.inc.sh"
+source "./scripts/inc-sh/common-post.inc.sh"
