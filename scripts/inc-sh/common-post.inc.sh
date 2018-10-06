d6c7117982c7d97ae695878f3a342dd99dc4f38f
Evgueni Souleimanov <esoule@100500.ca>
2018-10-06 13:07:45 -0400

    Run etckeeper at the end of scripts

--- scripts/inc-sh/common-post.inc.sh
+++ scripts/inc-sh/common-post.inc.sh
@@ -3,4 +3,7 @@ set +x
 echo "" >&2
 echo "" >&2
 cd "${PROJECT_HOME}"
+if which etckeeper >/dev/null ; then
+	etckeeper commit "committing changes in /etc after post-install-ubuntu $( basename "$0" )"
+fi
 true
