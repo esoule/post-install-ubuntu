0f76f3f4ae03bf5d23d46e6097e4916501f95e13
Evgueni Souleimanov <esoule@100500.ca>
2020-06-06 20:42:01 -0400

    Do not install coan package on 20.04 for now, it was not built yet

--- scripts/parts-post-install-ubuntu.d/510-apt-add-ppa.sh
+++ scripts/parts-post-install-ubuntu.d/510-apt-add-ppa.sh
@@ -6,11 +6,15 @@ require_root_or_exit
 
 main_func()
 {
+	local ubuntu_rel="$( lsb_release --short --release )"
+
 	add-apt-repository --yes ppa:git-core/ppa
 
-	add-apt-repository --yes ppa:esoule/esrepo
+	if ! [ "${ubuntu_rel}" = "20.04" ] ; then
+		add-apt-repository --yes ppa:esoule/esrepo
 
-	add-apt-repository --yes ppa:esoule/coan
+		add-apt-repository --yes ppa:esoule/coan
+	fi
 
 	apt -y update
 
