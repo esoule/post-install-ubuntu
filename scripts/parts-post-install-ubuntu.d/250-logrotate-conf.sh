917fd53be132a9d22e2fa803f4a08f0e8d423bf4
Evgueni Souleimanov <esoule@100500.ca>
2020-08-09 19:01:23 -0400

    250-logrotate-conf.sh: use install

--- scripts/parts-post-install-ubuntu.d/250-logrotate-conf.sh
+++ scripts/parts-post-install-ubuntu.d/250-logrotate-conf.sh
@@ -7,10 +7,13 @@ require_root_or_exit
 main_func()
 {
 	if ! [ -d /etc/logrotate.d ] ; then
-		install -v -d -m 0755 /etc/logrotate.d
+		install -v -d -m 0755 -o root -g root /etc/logrotate.d
 	fi
 
-	./scripts/helpers/install_file_attr.sh 0644 root root /etc/logrotate.d/100-local-dateext ./files/system/etc/logrotate.d/100-local-dateext
+	install -v -m 0644 -o root -g root \
+		./files/system/etc/logrotate.d/100-local-dateext \
+		/etc/logrotate.d/100-local-dateext
+
 }
 
 source "./scripts/inc-sh/common-run-main.inc.sh"
