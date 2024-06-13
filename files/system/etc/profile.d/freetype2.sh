82d47e0f7df810178e542bf3d6e1ada461838051
Evgueni Souleimanov <esoule@100500.ca>
2024-06-13 18:57:31 -0400

    Do not enable Freetype Classic subpixel mode in Ubuntu 24.04 LTS

--- files/system/etc/profile.d/freetype2.sh
+++
@@ -1,9 +0,0 @@
-# /etc/profile.d/freetype2.sh
-#
-# Classic subpixel mode (default in 2.6)
-#
-if [ -z "${FREETYPE_PROPERTIES}" ] ; then
-	FREETYPE_PROPERTIES="truetype:interpreter-version=35"
-	export FREETYPE_PROPERTIES
-fi
-
