d6e76faddc679588ed5a3abe2a2e41a070d2ae2f
Evgueni Souleimanov <esoule@100500.ca>
2024-06-18 19:27:16 -0400

    Replace which with command -v

--- scripts/inc-sh/common-post.inc.sh
+++ scripts/inc-sh/common-post.inc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set +x
 cd "${PROJECT_HOME}" || exit 1
-if which etckeeper >/dev/null ; then
+if command -v etckeeper >/dev/null ; then
 	etckeeper commit "committing changes in /etc after post-install-ubuntu $( basename "$0" )"
 fi
 printf '\n\n\n\n'    >&2
